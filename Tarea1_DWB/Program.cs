e30263e46727027a0e8f59ebf8b46eefb7317e53
PSRG12 <60238177+PSRG12@users.noreply.github.com>
2021-02-18 02:14:25 -0600

    first commit

---
+++ Tarea1_DWB/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Tarea1_DWB.DataAccess;
+using System.Linq;
+
+namespace Tarea1_DWB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NorthwindContext dbcontext = new NorthwindContext();
+
+            var employeeSelect = dbcontext.Employees.Select(s => new
+            {
+                //Proyección Lambda
+                EmployeeId = s.EmployeeId,
+                EmployeeFirstName = s.FirstName,
+                EmployeeAdress = s.Address,
+                EmployeeHomePhone = s.HomePhone,
+                EmployeeNotes = s.Notes
+
+            }).ToList();
+
+            Console.WriteLine("Proceso Exitoso:)");
+        }
+    }
+}
